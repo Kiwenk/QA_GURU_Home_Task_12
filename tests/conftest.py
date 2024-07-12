644369f6eac7ae9265e53cd589e4bc5930ba9acf
Katsuba Igor <kasubaigor@gmail.com>
2024-07-12 20:07:47 +0300

    3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,13 @@
-from selene import browser, Browser, Config
 import pytest
-from selenium.webdriver.chrome.options import Options
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
 from utils import attach
 
-
-@pytest.fixture(autouse=True)
-def browser_management():
-    browser.config.base_url = 'https://demoqa.com/'  # исправил
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
-    browser.config.window_width = 1200
-    browser.config.window_height = 1200
-
+@pytest.fixture(scope='function')
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -29,13 +23,12 @@ def browser_management():
         options=options
     )
 
-    BR = Browser(Config(driver))
+    browser = Browser(Config(driver))
+    yield browser
 
-    yield BR
-
-    # attach.add_screenshot(BR)
-    # attach.add_logs(BR)
-    # attach.add_html(BR)
-    # attach.add_video(BR)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
