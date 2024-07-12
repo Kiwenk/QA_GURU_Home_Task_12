243bd755b7bc4baa869d0859f9c3afcc27530bac
Katsuba Igor <kasubaigor@gmail.com>
2024-07-12 20:43:28 +0300

    18

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,12 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(autouse=True)
 def browser_management():
-    #browser.config.base_url = 'https://demoqa.com/'  # исправил
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
+    browser.config.base_url = 'https://demoqa.com/'
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.add_argument('--headless')
+    # browser.config.driver_options = driver_options
     browser.config.window_width = 1200
     browser.config.window_height = 1200
-
 
 
 @pytest.fixture(scope='function')
@@ -35,8 +34,7 @@ def setup_browser(request):
     )
 
     browser = Browser(Config(driver))
-
-    yield
+    yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
